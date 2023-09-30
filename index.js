d476517f54e41bcb17615c99c17dd57413153fe4
clairemuiru1 <muiruclaire@gmail.com>
2023-09-30 07:52:34 +0300

    "completed assignment"

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newheader = document.createElement('H1')
+newheader.id = 'Victory'
+newheader.innerHTML = "YOUR-NAME is the champion"
